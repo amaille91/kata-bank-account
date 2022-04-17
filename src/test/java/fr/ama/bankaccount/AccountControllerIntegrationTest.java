db8ea5b3d9c2dfcd7b9a9a236c0532a2b004318a
Alexandre Maille <al.maille@laposte.net>
2022-04-17 19:02:57 +0200

    TDD for account creation (PUT /account)

---
+++ src/test/java/fr/ama/bankaccount/AccountControllerIntegrationTest.java
@@ -0,0 +1,39 @@
+package fr.ama.bankaccount;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class AccountControllerIntegrationTest {
+
+	private MockMvc mockMvc;
+	private ObjectMapper objectMapper = new ObjectMapper();
+
+	@Autowired
+	public AccountControllerIntegrationTest(MockMvc mockMvc, ObjectMapper objectMapper) {
+		this.mockMvc = mockMvc;
+		this.objectMapper = objectMapper;
+	}
+
+	@Test
+	void putting_an_account_should_gives_us_an_account_with_a_balance_of_0() throws Exception {
+		String putResponseBody = mockMvc.perform(MockMvcRequestBuilders.put("/account"))
+				.andExpect(status().is2xxSuccessful())
+				.andReturn()
+				.getResponse().getContentAsString();
+		Account account = objectMapper.readValue(putResponseBody, Account.class);
+
+		assertThat(accountBalance.getId()).isNotNull();
+		assertThat(accountBalance.getBalance()).isEqualTo(0);
+	}
+}
