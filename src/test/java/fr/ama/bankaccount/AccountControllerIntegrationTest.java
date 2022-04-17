391dfb99fedf5b53d4c8aeb410aa2c28972f6f6f
Alexandre Maille <al.maille@laposte.net>
2022-04-17 19:29:20 +0200

    TDD - deposit in an account should increase the account's balance

--- src/test/java/fr/ama/bankaccount/AccountControllerIntegrationTest.java
+++ src/test/java/fr/ama/bankaccount/AccountControllerIntegrationTest.java
@@ -1,6 +1,7 @@
 package fr.ama.bankaccount;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.jupiter.api.Test;
@@ -29,7 +30,7 @@ class AccountControllerIntegrationTest {
 
 	@Test
 	void putting_an_account_should_gives_us_an_account_with_a_balance_of_0() throws Exception {
-		String putResponseBody = mockMvc.perform(MockMvcRequestBuilders.put("/account"))
+		String putResponseBody = mockMvc.perform(put("/account"))
 				.andExpect(status().is2xxSuccessful())
 				.andReturn()
 				.getResponse().getContentAsString();
@@ -37,5 +38,25 @@ class AccountControllerIntegrationTest {
 
 		assertThat(account.getId()).isNotNull();
 		assertThat(account.getBalance()).isEqualTo(0);
+	}
+
+	// TODO Test for int overflow or use BigInteger
+	@Test
+	void putting_a_deposit_on_an_existing_account_should_give_back_the_new_account_with_the_balance_increased()
+			throws Exception {
+		String createAccountResponse = mockMvc.perform(MockMvcRequestBuilders.put("/account"))
+				.andExpect(status().is2xxSuccessful())
+				.andReturn()
+				.getResponse().getContentAsString();
+		Account initialAccount = objectMapper.readValue(createAccountResponse, Account.class);
+
+		String depositResponse = mockMvc.perform(put("account/" + initialAccount.getId() + "/deposit/1000"))
+				.andExpect(status().is2xxSuccessful())
+				.andReturn()
+				.getResponse().getContentAsString();
+		Account newAccount = objectMapper.readValue(depositResponse, Account.class);
+
+		assertThat(newAccount.getId()).isEqualTo(initialAccount.getId());
+		assertThat(newAccount.getBalance()).isEqualTo(1000);
 	}
 }
